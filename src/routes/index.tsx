cd5c96c79ce598da01cae534cc1a831230fb1c80
Mitesh Ashar <email@miteshashar.com>
2023-12-21 17:01:03 +0530

    Added left alignment to questions in the FAQ section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -135,7 +135,7 @@ export default function Home() {
             <>
               <h2 id={`faqs-heading-${faq.id}`}>
                 <button type="button" class={`flex items-center justify-between w-full p-5 font-medium border border-gray-300 focus:ring-4 focus:ring-gray-300 hover:bg-gray-100 gap-3 ${i()==0?"rounded-t-xl":""}`} data-accordion-target={`#faqs-body-${faq.id}`} aria-expanded="false" aria-controls={`faqs-body-${faq.id}`}>
-                  <span>{faq.question}</span>
+                  <span class="text-left">{faq.question}</span>
                   <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M9 5 5 1 1 5"/>
                   </svg>
