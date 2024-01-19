193928fb148bc1c4c585a5a616d7f514dc99a485
Mitesh Ashar <email@miteshashar.com>
2024-01-19 15:45:40 +0530

    Dependencies for MDX, which will be used to introduce blog

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "@solidjs/start/config";
 
+import pkg from "@vinxi/plugin-mdx";
+const {default: mdx} = pkg;
+
 export default defineConfig({
   start: {
+    extensions: ["mdx", "md"],
     ssr: true,
     server: {
       baseURL: process.env.BASE_PATH,
       preset: "static"
     }
-  }
+  },
+  plugins: [
+    mdx.withImports({})({
+      jsx: true,
+      jsxImportSource: "solid-js",
+      providerImportSource: "solid-mdx",
+    })
+  ]
 });
